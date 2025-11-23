31c31d2efaac0cf2e1009d3a2e18d5b709348ffb
aaarif796 <aaarif796@gmail.com>
2025-11-23 16:56:34 +0530

    updated

--- echospeak/speech/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SpeechAnalysis
-
-class SpeechAnalysisSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SpeechAnalysis
-        fields = '__all__'
